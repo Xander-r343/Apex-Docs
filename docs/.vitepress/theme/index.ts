6d7d7f38116d4848b383c0bcfdfc1ac4f734db86
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-03-24 15:46:01 -0700

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,13 +2,16 @@ import DefaultTheme from 'vitepress/theme'
 import CustomNavButton from './CustomNavButton.vue'
 import './custom.css'
 import { h } from 'vue'
+import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      // You can add your custom button to the 'nav-bar-content-after' or 'nav-bar-content-before' slot
       'nav-bar-content-after': () => h(CustomNavButton)
     })
+  },
+  enhanceApp({ app }) {
+    enhanceAppWithTabs(app)
   }
 }
